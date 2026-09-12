bd969d8ee4b543512a1d333393da689241ed4ba5
C8suco <cjw2006@foxmail.com>
2026-09-12 20:51:04 +0800

    添加NRF24L01数据解包

---
+++ FlightControl/drivers/NRF24L01/Com_Config.c
@@ -0,0 +1,19 @@
+#include "Com_Config.h"
+#include "NRF24L01_Bridge.h"
+extern uint8_t Buf[NRF24L01_Buf_Len];
+void Com_Limit(float *inner, float Max, float Min) {
+  if (*inner > Max) {
+    *inner = Max;
+  }
+  if (*inner < Min) {
+    *inner = Min;
+  }
+}
+void Com_NRF_Access(Remote_data *data,uint8_t *Buf) {
+  data->pit = (uint16_t)(Buf[6] | (Buf[7] << 8));
+  data->rol = (uint16_t)(Buf[8] | (Buf[9] << 8));
+  data->thr = (uint16_t)(Buf[10] | (Buf[11] << 8));
+  data->yaw = (uint16_t)(Buf[12] | (Buf[13] << 8));
+  data->shortdown = Buf[14];
+  data->fix_high = Buf[15];
+}
